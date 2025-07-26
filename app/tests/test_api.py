a0ab3b01f15e068e6c6a7a2a07e30c347c085600
Roberto Devesa Fernández <15369573+Roberdvs@users.noreply.github.com>
2025-07-26 16:42:07 +0200

    Update unit tests

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -1,14 +1,34 @@
-import pytest
-from httpx import AsyncClient
+from fastapi.testclient import TestClient
+from sqlmodel import SQLModel, create_engine, Session
+from sqlmodel.pool import StaticPool
 from app.main import app
+from app import database
 
-@pytest.mark.asyncio
-async def test_put_and_get_hello(monkeypatch):
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        # Insert user
-        resp = await ac.put("/hello/testuser", json={"dateOfBirth": "2000-01-01"})
-        assert resp.status_code == 204
-        # Get user
-        resp = await ac.get("/hello/testuser")
-        assert resp.status_code == 200
-        assert "Hello, testuser!" in resp.json()["message"] 
+# Create a new in-memory SQLite engine for testing
+engine = create_engine(
+    "sqlite://",
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+
+# Create the tables
+SQLModel.metadata.create_all(engine)
+
+
+# Dependency override
+def get_session_override():
+    with Session(engine) as session:
+        yield session
+
+
+app.dependency_overrides[database.get_session] = get_session_override
+
+client = TestClient(app)
+
+
+def test_put_and_get_hello():
+    resp = client.put("/hello/testuser", json={"dateOfBirth": "2000-01-01"})
+    assert resp.status_code == 200
+    resp = client.get("/hello/testuser")
+    assert resp.status_code == 200
+    assert "Hello, testuser!" in resp.json()["message"]
